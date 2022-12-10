0834f67acc4bffb49fd068b3d239588fc01d8224
LucyShine01 <saasdfhr@gmail.com>
2022-12-10 23:33:03 +0900

    2022-12-10 jsp프로젝트 1일자 2

--- works/JSP_Project_Front/viewPage.js
+++ works/JSP_Project_Front/viewPage.js
@@ -1,13 +1,24 @@
 'use strict';
 let fixSW = 0;
+let randomSearch = Math.floor((Math.random() * 12) +1)
+if(randomSearch == 1) $("#search2").val("주거공간 인테리어");
+else if(randomSearch == 2) $("#search2").val("상업공간 인테리어");
+else if(randomSearch == 3) $("#search2").val("조명");
+else if(randomSearch == 4) $("#search2").val("욕실 화장실");
+else if(randomSearch == 5) $("#search2").val("타일시공");
+else if(randomSearch == 6) $("#search2").val("페인트시공");
+else if(randomSearch == 7) $("#search2").val("싱크대 교체");
+else if(randomSearch == 8) $("#search2").val("도배장판");
+else if(randomSearch == 9) $("#search2").val("필름교체");
+else if(randomSearch == 10) $("#search2").val("도면 제작 수정");
+else if(randomSearch == 11) $("#search2").val("3D모델링");
+else if(randomSearch == 12) $("#search2").val("인테리어 컨설팅");
 // 요소나온후 미리 읽어와야함
 window.onload = function() {
   let loadingBar = document.getElementById("loading_Bar");
   if(isNaN(loadingBar))fixSW = 1;
   else fixSW = 0;
   $(window).scroll(function(){
-  console.log($(this).scrollTop());
-  console.log(document.getElementById("headMain").clientHeight + "px");
   let space = document.getElementById("headMain").clientHeight + "px";
   if(fixSW == 1){
     if($(this).scrollTop() > 60) {
@@ -33,11 +44,26 @@ $(document).ready(function(){
   $("#loading_Bar").slideUp();
   $("#headerTop").delay(700);
   $("#headerTop").slideDown(700);
-
+  $("#mainView").css("background-color","#000225");
   // carousel
   $("#slideImg").on('slide.bs.carousel', function(e){
     let val = e.to;
     $("#numIns").html("0"+(val+1));
+    if(val == 0) {
+      $("#mainView").css("background-color","#000225");
+    }
+    else if(val == 1) {
+      $("#mainView").css("background-color","#1e44a6");
+    }
+    else if(val == 2) {
+      $("#mainView").css("background-color","#303441");
+    }
+    else if(val == 3) {
+      $("#mainView").css("background-color","#37116c");
+    }
+    else if(val == 4) {
+      $("#mainView").css("background-color","#ddd");
+    }
   });
   $('#slideImg').carousel({
     // 슬리아딩 자동 순환 지연 시간
